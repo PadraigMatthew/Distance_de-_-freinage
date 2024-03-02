5a1f875741e80a9a9d02640be7492cdc87477ed6
PadraigMatthew <145544247+PadraigMatthew@users.noreply.github.com>
2024-03-02 15:57:51 +0100

    Test

---
+++ dd.py
@@ -0,0 +1,16 @@
+
+
+Sec = 1
+Mouil = 1.5
+
+print("Distance de freinage")
+pdsDuVehicule = float(input("Entrée le poids de votre véhicule ? "))
+b= (pdsDuVehicule//10)
+c= b
+a= b*c
+
+retour_a_la_phrase = str(a)
+print ("distance de freinage à respecté" + (retour_a_la_phrase) +"mètres")
+
+
+
